c184145199a711789eb31f5afe791bc1e7dde32c
Den Durruty <dendurruty@users.noreply.github.com>
2025-02-18 06:00:15 -0300

    Mejoras y refactorización del método register
    1) Se agregó una variable let = 3 para que por defecto los registros sean de tipo cliente.
    
    2) Se eliminó idUsuarioTipo como dato obligatorio.
    
    3) Se corrigió la verificación de existencia de usuario. Faltaba instanciar la clase.
    
    4) Se ajustaron los datos que devuelve al completar el registro. Devuelve: mensaje de confirmación de registro, correo electrónico y token.
    
    5) Se agregó la validación de contraseña por fuerza con un mínimo de 8 caracteres, incluyendo al menos una mayúscula, una minúscula, un número y un carácter especial, utilizando un patrón regex.
    
    6) Se comentó todo el código.

--- backend-concesionaria-prog3/src/controllers/authController.js
+++ backend-concesionaria-prog3/src/controllers/authController.js
@@ -13,38 +13,54 @@ export default class AuthController {
 
             const { nombre, apellido, correoElectronico, contrasenia, idUsuarioTipo, imagen } = req.body;
     
-            
-            if (!nombre || !apellido || !correoElectronico || !contrasenia || !idUsuarioTipo) {
+            // Limitar datos obligatorios
+            if (!nombre || !apellido || !correoElectronico || !contrasenia) {
                 return res.status(400).json({ mensaje: 'Faltan datos obligatorios para el registro.' });
             }
-/*
-            const usuarioExiste = await UsuariosService.buscarPorEmail(correoElectronico);
+
+            // Validar fuerza de la contraseña
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.,])[A-Za-z\d@$!%*?&.,]{8,}$/;
+
+            if (!passwordRegex.test(contrasenia)) {
+                return res.status(400).json({
+                    mensaje: 'La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula, un número y un carácter especial.'
+                });
+                
+            }
+
+            // Verificar existencia de usuario
+            const usuarioPorEmail = new UsuariosService();
+            const usuarioExiste = await usuarioPorEmail.buscarPorEmail(correoElectronico);
             console.log("usuarioExiste:", usuarioExiste);
 
             if (usuarioExiste) {
-                return res.status(409).json({ message: 'Este usuario ya existe' });
+                return res.status(409).json({ message: 'Este usuario ya existe.' });
             }
-*/    
+        
+            // Determinar el tipo de usuario: cliente, por defecto
+            let tipoUsuario = 3;
            
+            // Hashear contraseña
             const hashedPassword = await bcrypt.hash(contrasenia, 10);
            
-            const usuariosService = new UsuariosService();
-    
-            const usuarioNuevo = await usuariosService.registrar({
+            // Registrar usuario
+            const usuariosRegis = new UsuariosService();
+            const usuarioNuevo = await usuariosRegis.registrar({
                 nombre,
                 apellido,
                 correoElectronico,
                 contrasenia: hashedPassword,
-                idUsuarioTipo,
+                idUsuarioTipo: tipoUsuario,
                 imagen
             });
     
             if (!usuarioNuevo) {
                 return res.status(500).json({ mensaje: 'Error al registrar el usuario.' });
             }
-
+            
+            // Generar token para el nuevo
             const token = jwt.sign(usuarioNuevo, process.env.JWT_SECRET, { expiresIn: '1h' });
-            return res.status(201).json({ mensaje: 'Usuario registrado con éxito.', usuario: usuarioNuevo, token: token });
+            return res.status(201).json({ mensaje: 'Usuario registrado con éxito.', correoElectronico, token: token });
 
         } catch (error) {
             console.error('Error en el controlador de registro:', error);
