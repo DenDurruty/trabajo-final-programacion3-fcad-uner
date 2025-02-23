281a71c136887d28ed61b22e486757b5e6a6ffaf
Den Durruty <dendurruty@users.noreply.github.com>
2025-02-23 00:04:11 -0300

    Se eliminaron notas y código comentado que ya no es necesario.

--- backend-concesionaria-prog3/src/services/reclamosEstadosService.js
+++ backend-concesionaria-prog3/src/services/reclamosEstadosService.js
@@ -1,8 +1,5 @@
 import ReclamosEstados from '../database/reclamosEstados.js';
 
-// Crear clase para definir los métodos que vamos a conectar
-// Vamos a tener un constructor donde instanciaremos nuestra clase de base de datos para poder trabajar con sus métodos
-// Vamos a utilizar esa instancia para BUSCAR todos los reclamos de estado
 export default class ReclamosEstadosService{
 
     constructor() {
@@ -24,17 +21,6 @@ export default class ReclamosEstadosService{
     modificar = (reclamoEstado) => {
         return this.reclamosEstados.modificar(reclamoEstado)
     }
-/*
-    cancelar = (idReclamoEstado) => {
-        return this.reclamosEstados.cancelar(idReclamoEstado);
-    }
-*/
- // Cuando utilicemos este servicio va a existir un método que me busca todos los reclamos de estado
-/*
-    consultarEstado = (consultarEstadoReclamo) => {
-        return this.reclamosEstados.consultarEstado(consultarEstadoReclamo);
-    } 
-*/   
-    
+ 
 }
 
