f33501406bcf0c2828914918c1e298f35482ed36
Den Durruty <dendurruty@users.noreply.github.com>
2025-02-26 04:31:34 -0300

    Se instaló Swagger para documentar la API.

--- backend-concesionaria-prog3/src/index.js
+++ backend-concesionaria-prog3/src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { conn } from './database/conn.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from "swagger-ui-express";
+//import swaggerJsdoc from "swagger-jsdoc";
+import YAML from 'yamljs';
+
 
 import { router as v1ReclamosTiposRouter } from './v1/routes/reclamosTiposRoutes.js';
 import { router as v1ReclamosEstadoRouter } from './v1/routes/reclamosEstadosRoutes.js';
@@ -14,11 +17,10 @@ import { router as v1AuthRouter } from './v1/routes/authRoutes.js';
 //import validateContentType from './middlewares/validateContentType.js';
 import autenticarUsuario from './middlewares/autenticarUsuarios.js';
 
+
 import fs from 'fs';
 import path  from 'path';
 import { fileURLToPath } from 'url';
-
-
 
 
 dotenv.config();
@@ -31,7 +33,8 @@ app.use(express.json());
 app.use(cors()); 
 //app.use(validateContentType);
 
-// morgan
+
+// Morgan
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'accesos.log'), { flags: 'a' });
@@ -39,29 +42,26 @@ app.use(morgan('combined', { stream: accessLogStream }));
 
 
 // Rutas
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.send('Gestión de reclamos')
-    //res.json({'estado':true});
+   
 });
 
+app.use('/api/v1/auth', v1AuthRouter);
+app.use('/api/v1/usuarios', autenticarUsuario, v1UsuariosRouter);
+app.use('/api/v1/oficinas', autenticarUsuario, v1OficinasRouter);
+//app.use('/api/v1/reclamos', v1ReclamosRouter);
+app.use('/api/v1/reclamos', autenticarUsuario, v1ReclamosRouter);
 app.use('/api/v1/reclamos-tipos', autenticarUsuario, v1ReclamosTiposRouter);
 app.use('/api/v1/reclamos-estados', autenticarUsuario, v1ReclamosEstadoRouter);
-//app.use('/api/v1/reclamos', autenticarUsuario, v1ReclamosRouter);
-app.use('/api/v1/reclamos', v1ReclamosRouter);
-app.use('/api/v1/oficinas', autenticarUsuario, v1OficinasRouter);
-app.use('/api/v1/usuarios', autenticarUsuario, v1UsuariosRouter);
-app.use('/api/v1/auth', v1AuthRouter);
 
 
-// Probar conexión a base de datos
-conn.getConnection(err => {
-    if (err) {
-        console.error('❌ Error conectando a la base de datos:', err.message);
-        process.exit(1);
-    } else {
-        console.log('✅ Conexión exitosa a MySQL');
-    }
-});
+// Swagger 
+//const swaggerDocs = swaggerJsdoc(swaggerOptions);
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Iniciar el puerto
 const PUERTO = process.env.PUERTO || 3000;
